f4ba65d3fbdf8625dd6ea3cb578c641b45f195bd
Spencer Hansen <spencerhansen47@gmail.com>
2014-09-09 15:36:10 -0500

    Revert "Adding"
    This reverts commit f92ba137fcb559e589f07e773642502188d727fd.

--- drunks.py
+++
@@ -1,8 +0,0 @@
-import csv
-
-reader = csv.reader(open("drunks.csv", "rU"),dialect=csv.excel)
-
-reader.next()
-
-for row in reader:
-    print row[0], "is a real drunk. They drank", row[1], "drinks in one sitting one time."
--- madlib.py
+++
@@ -1,9 +0,0 @@
-noun = "video games"
-verb = "play"
-adjective = "really tall"
-proper_name = "Spencer"
-quantity = 21
-
-mylist = [39, 21, 21, 21, 25, 24, 22]
-
-print "My name is %s. I am %i years old and I am %s. I like to %s %s." % (proper_name, quantity, adjective, verb, noun)
