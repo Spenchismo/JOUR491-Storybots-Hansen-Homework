2b1f82836fcef724b26042f11cdc5dd791aec994
Spencer Hansen <spencerhansen47@gmail.com>
2014-09-09 15:36:16 -0500

    Revert "Revert "Adding""
    This reverts commit f4ba65d3fbdf8625dd6ea3cb578c641b45f195bd.

---
+++ madlib.py
@@ -0,0 +1,9 @@
+noun = "video games"
+verb = "play"
+adjective = "really tall"
+proper_name = "Spencer"
+quantity = 21
+
+mylist = [39, 21, 21, 21, 25, 24, 22]
+
+print "My name is %s. I am %i years old and I am %s. I like to %s %s." % (proper_name, quantity, adjective, verb, noun)
